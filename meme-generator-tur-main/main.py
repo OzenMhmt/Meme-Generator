cf925946433d78e75b38612ebe6eb3632b1e2a19
Mehmet Özen <167196937+OzenMhmt@users.noreply.github.com>
2024-06-28 19:33:23 +0300

    Add files via upload

---
+++ meme-generator-tur-main/main.py
@@ -0,0 +1,47 @@
+# İçeri Aktar
+from flask import Flask, render_template, request, send_from_directory
+
+
+app = Flask(__name__)
+
+# Form sonuçları 
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        # seçilen resmi almak
+        selected_image = request.form.get('image-selector')
+
+        # Görev #2. Metni almak
+        text_top = request.form.get('textTop')
+        text_bottom = request.form.get('textBottom')
+        
+        # Görev #3. Metnin konumunu almak
+        selected_color = request.form.get('color-selector')
+
+        # Görev #3. Metnin rengini almak
+        
+
+        return render_template('index.html', 
+                               # Seçilen resmi görüntüleme
+                               selected_image=selected_image, 
+
+                               # Görev #2. Metni görüntüleme
+                               text_top = text_top,
+                               
+
+                               # Görev #3. Rengi görüntüleme
+                               selected_color = selected_color
+                               
+                               # Görev #3. Metnin konumunu görüntüleme
+
+                               )
+    else:
+        # Varsayılan olarak ilk resmi görüntüleme
+        return render_template('index.html', selected_image='logo.svg')
+
+
+@app.route('/static/img/<path:path>')
+def serve_images(path):
+    return send_from_directory('static/img', path)
+
+app.run(debug=True)
